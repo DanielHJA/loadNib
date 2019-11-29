cd6b2e72ed92e425484310b2985a174d0bdf9b69
Daniel <daniel.hjartstrom@gmail.com>
2019-11-29 08:46:36 +0100

    Initial Commit

---
+++ UIViewNibTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIViewNibTest
+//
+//  Created by Daniel Hjärtström on 2019-11-29.
+//  Copyright © 2019 Daniel Hjärtström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
